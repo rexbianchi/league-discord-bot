d9cf7d36827e59372c4ddbe68170033e35401680
Rex Bianchi <rexbianchi@protonmail.com>
2021-10-29 13:34:35 -0400

    testing bot code

---
+++ Bot/discordBot.py
@@ -0,0 +1,19 @@
+import discord
+import os
+
+from dotenv import load_dotenv
+
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+bot = commands.Bot(command_prefix="!")
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} has connected to Discord!")
+
+
+bot.run(TOKEN)
